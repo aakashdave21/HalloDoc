d42da2623bbf0dd15cc2323682c9ea25bcdaac35
aakashdave21 <aakash.dave@etatvasoft.com>
2024-01-19 14:46:28 +0530

    all task are done 19/1/2024

--- Patient-Dashboard/js/global.js
+++ Patient-Dashboard/js/global.js
@@ -10,3 +10,16 @@ $("#theme-btn").click(function(){
         $("#theme-btn .fa-sun").css("display","none")
     }
 })
+
+const contactWithFlag = (fieldWithId)=>{
+    const phoneInput = window.intlTelInput(fieldWithId, {
+        preferredCountries: [ "in","us", "au", "ru"],
+        utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+}
+
+const phoneInputField = document.querySelector("#phone");
+const phoneInputPatientField = document.querySelector("#patient-phone");
+contactWithFlag(phoneInputField)
+contactWithFlag(phoneInputPatientField)
